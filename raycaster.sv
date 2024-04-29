6bcbc5e484f36ea50b35ec15162000d727a170ad
Dylan Dang <endercraft2319@gmail.com>
2024-04-29 17:38:08 -0500

    implement trig LUTs

--- raycaster.sv
+++ raycaster.sv
@@ -1,11 +1,31 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+`define fmask 32'h0000ffff // fractional part
+`define imask 32'hffff0000 // integer part
 
-// Q8.8 fixed point number
-typedef logic signed [15:0] fix_t;
+// Q16.16 fixed point number
+typedef logic signed [31:0] fix_t;
 
-module raytracer (  // coordinate width
+function fix_t to_fix(input real real_num);
+    begin
+        to_fix = fix_t'($rtoi(real_num * 2**16));
+    end
+endfunction
+
+function real to_real(input fix_t fix_num);
+    begin
+        to_real = $itor(32'(fix_num)) / 2**16;
+    end
+endfunction
+
+function fix_t mult(input fix_t a, input fix_t b);
+    begin
+        mult = 32'((48'(a * b)) >>> 16);
+    end
+endfunction
+
+module raycaster (  // coordinate width
     input  wire logic clk_in,             // pixel clock
     input  wire logic rst_in,             // sim reset
     input  wire logic[3:0] mvmt_in,       // player movement
@@ -37,17 +57,71 @@ module raytracer (  // coordinate width
     logic frame;  // high for one tick at the start of vertical blanking
     always_comb frame = (sy == V_RES && sx == 0);
 
-    /* --------------------------- sine LUT --------------------------- */
+    /* --------------------------- trig LUT --------------------------- */
+    // feature idea: lerp between entries
 
-    localparam SPEED = 5;
-    localparam ANGLES = 256;
-    fix_t sine_table[ANGLES];
+    localparam SAMPLES = 256;
+    fix_t sin_table[SAMPLES];
+    fix_t sec_table[SAMPLES];
     localparam PI = 3.14159265358979323846;
     generate
-        for(genvar i = 0; i < ANGLES; i++) begin
-            assign sine_table[i] = fix_t'($rtoi((SPEED*$sin(2*PI * i/ANGLES)) * 2**8));
+        for(genvar i = 0; i < SAMPLES; i++) begin
+            assign sin_table[i] = to_fix($sin(PI/2 * i/SAMPLES));
+            assign sec_table[i] = to_fix(1/$cos(PI/2 * i/SAMPLES));
         end
     endgenerate
+
+    function fix_t sin(input fix_t x);
+        begin
+            fix_t quad = mult(x, to_fix(2 / PI));
+            fix_t entry = mult(quad & `fmask, to_fix(SAMPLES));
+            logic [$clog2(SAMPLES)-1:0] index = entry[15+$clog2(SAMPLES):16];
+            case (quad[17:16] /*int part mod 4*/)
+                2'd0: sin = sin_table[index];
+                2'd1: sin = sin_table[SAMPLES - index];
+                2'd2: sin = -sin_table[index];
+                2'd3: sin = -sin_table[SAMPLES - index];
+            endcase
+        end
+    endfunction
+    
+    function fix_t sec(input fix_t x);
+        begin
+            fix_t quad = mult(x, to_fix(2 / PI));
+            fix_t entry = mult(quad & `fmask, to_fix(SAMPLES));
+            logic [$clog2(SAMPLES)-1:0] index = entry[15+$clog2(SAMPLES):16];
+            case (quad[17:16] /*int part mod 4*/)
+                2'd0: sec = sec_table[index];
+                2'd1: sec = -sec_table[SAMPLES - index];
+                2'd2: sec = sec_table[SAMPLES - index];
+                2'd3: sec = -sec_table[index];
+            endcase
+        end
+    endfunction
+
+    function fix_t cos(input fix_t x);
+        begin
+            cos = sin(x + to_fix(PI/2));
+        end
+    endfunction
+
+    function fix_t csc(input fix_t x);
+        begin
+            csc = sec(x + to_fix(3*PI/2));
+        end
+    endfunction
+
+    function fix_t tan(input fix_t x);
+        begin
+            tan = mult(sin(x), sec(x));
+        end
+    endfunction
+
+    function fix_t cot(input fix_t x);
+        begin
+            tan = mult(cos(x), csc(x));
+        end
+    endfunction
 
     /* --------------------------- Map --------------------------- */
 
@@ -60,32 +134,47 @@ module raytracer (  // coordinate width
 
     /* --------------------------- Movement --------------------------- */
 
+    localparam SPEED = to_fix(5);
+    localparam TURN_SPEED = to_fix(0.1);
     localparam INIT_ANGLE = PI / 2;
-    logic[$clog2(ANGLES)-1:0] pa = ($clog2(ANGLES))'($rtoi(INIT_ANGLE/(2*PI)*ANGLES));
-    fix_t px = 16'd20 << 8;
-    fix_t py = 16'd20 << 8;
-    fix_t pdx = fix_t'($rtoi((SPEED*$cos(INIT_ANGLE)) * 2**8));
-    fix_t pdy = fix_t'($rtoi((SPEED*$sin(INIT_ANGLE)) * 2**8));
-    wire forward, backward, left, right;
-    assign { forward, backward, left, right } = mvmt_in;
+
+    fix_t pa = to_fix(INIT_ANGLE);
+    fix_t px = to_fix(1);
+    fix_t py = to_fix(1);
+    fix_t pdx = to_fix(SPEED*$cos(INIT_ANGLE));
+    fix_t pdy = to_fix(SPEED*$sin(INIT_ANGLE));
+    wire key_up, key_down, key_left, key_right;
+    assign { key_up, key_down, key_left, key_right } = mvmt_in;
 
     always_ff @(posedge clk_in) begin
         if (frame) begin
-            if (left || right) begin
-                pa = pa - ($clog2(ANGLES))'(left) + ($clog2(ANGLES))'(right);
-                pdx <= sine_table[pa + ANGLES/4];
-                pdy <= sine_table[pa];
+            if (key_left) begin
+                pa += TURN_SPEED;
+                if (pa < to_fix(0)) pa+=to_fix(2*PI);
+                pdx <= mult(SPEED, cos(pa));
+                pdy <= mult(SPEED, sin(pa));
             end
-            if (forward) begin
+            if (key_right) begin
+                pa -= TURN_SPEED;
+                if (pa > to_fix(2*PI)) pa-=to_fix(2*PI);
+                pdx <= mult(SPEED, cos(pa));
+                pdy <= mult(SPEED, sin(pa));
+            end
+            if (key_up) begin
                 px += pdx;
                 py += pdy;
             end
-            if (backward) begin
+            if (key_down) begin
                 px -= pdx;
                 py -= pdy;
             end
         end
     end
+
+    /* --------------------------- Raycasting --------------------------- */
+    
+    localparam FOV = to_fix(PI/3); // 60deg
+
 
     /* --------------------------- Rendering --------------------------- */
 
@@ -93,8 +182,8 @@ module raytracer (  // coordinate width
         logic square, square_2;
         always_comb begin
             /* verilator lint_off WIDTH */
-            square = (sx >= (px >> 6)) && (sx < (px >> 6) + Q_SIZE) && (sy >= (py >> 6)) && (sy < (py >> 6) + Q_SIZE);
-            square_2 = (sx >= ((px+pdx) >> 6)) && (sx < ((px+pdx) >> 6) + Q_SIZE) && (sy >= ((py+pdy) >> 6)) && (sy < ((py+pdy) >> 6) + Q_SIZE);
+            square = (sx >= (px >> 16)) && (sx < (px >> 16) + Q_SIZE) && (sy >= (py >> 16)) && (sy < (py >> 16) + Q_SIZE);
+            square_2 = (sx >= ((px+pdx) >> 16)) && (sx < ((px+pdx) >> 16) + Q_SIZE) && (sy >= ((py+pdy) >> 16)) && (sy < ((py+pdy) >> 16) + Q_SIZE);
 
         end
 
