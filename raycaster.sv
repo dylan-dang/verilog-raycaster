e783fe3584dc0b46eb0742973d8f90d6a948fadf
Dylan Dang <endercraft2319@gmail.com>
2024-05-01 09:17:53 -0500

    add comments

--- raycaster.sv
+++ raycaster.sv
@@ -1,28 +1,28 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-`define LEVEL     "levels/8x8/simple.mem"
-`define MAP_X     8
-`define MAP_Y     8
-`define MAP_SCALE_X 64
-`define MAP_SCALE_Y 64
-`define MAP_SCALE_Z 64
+`define LEVEL       "levels/8x8/multi.mem"
+`define MAP_X       8  
+`define MAP_Y       8 
+`define MAP_SCALE_X 64.0
+`define MAP_SCALE_Y 64.0
+`define MAP_SCALE_Z 64.0
 `define MAP_WRAP
 
 `define TEX_X 256
 `define TEX_Y 256
 
 `define PLAYER_SPEED      2.0
-`define PLAYER_TURN_SPEED 0.05
+`define PLAYER_TURN_SPEED 0.05 // radians
 `define PLAYER_INIT_X     (MAP_SCALE_X * MAP_X / 2)
 `define PLAYER_INIT_Y     (MAP_SCALE_Y * MAP_Y / 2)
 `define PLAYER_INIT_ANGLE 0.0
 
-`define FOV (PI / 3)
+`define FOV (PI / 3) // 60deg
 
 `define TRIG_SAMPLES 256
 
-`define MAP_OVERLAY
+// `define MAP_OVERLAY
 `define OVERLAY_SCALE_X     0.5
 `define OVERLAY_SCALE_Y     0.5
 `define OVERLAY_OFFSET_X    0.0
